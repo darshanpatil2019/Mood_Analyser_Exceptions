79a8a3168ef67532c1ba6cd75e913d51b8e0991b
Darshan Patil <iampatildarsh>
2023-01-10 16:54:40 +0530

    To Handle NullPointerException

--- Exceptions/src/main/java/org/example/MoodAnalyserProblem.java
+++ Exceptions/src/main/java/org/example/MoodAnalyserProblem.java
@@ -1,33 +1,37 @@
 package org.example;
 
 public class MoodAnalyserProblem {
-   //Declaring Variables
+    //Declaring Variables
     String message;
     String mood;
 
     //Default Constructor
-    public MoodAnalyserProblem(){
-        message="";
+    public MoodAnalyserProblem() {
+        message = "";
 
     }
+
     //Parameterized Constructor to pass message as parameter
     public MoodAnalyserProblem(String message) {
         this.message = message;
     }
 
-    //Method to Analyse the mood
-    public String analyseMood() {
-        if(message.contains("Happy"))
-        {
-            mood = "Happy";
-        } else if(message.contains("Sad"))
-        {
-            mood= "Sad";
-        }
-        return mood;
+    public static void main(String[] args) {
+        System.out.println("Welcome to Mood Analyzer Problem");
     }
 
-    public static void main (String[] args){
-        System.out.println("Welcome to Mood Analyzer Problem");
+    //Method to Analyse the mood
+    public String analyzeMood() {
+        {
+            try {
+                if (message.contains("Happy")) {
+                    return "Happy";
+                } else {
+                    return "Sad";
+                }
+            } catch (NullPointerException e) {
+                return "Happy";
+            }
+        }
     }
 }
